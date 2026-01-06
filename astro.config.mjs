69ff9edd4a175d7e9ae6a84ba8bbca6a1548bf7b
Klabel <neveroff606@gmail.com>
2026-01-06 16:09:05 +0500

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,9 @@
 
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  // Убедитесь, что Tailwind подключен здесь
-  integrations: [tailwind()],
-
-  // Убедитесь, что здесь НЕТ старой секции vite: { plugins: [tailwindcss()] }
+  site: 'https://lifter.kz', // Замените на ваш реальный домен!
+  integrations: [tailwind(), sitemap()],
 });
